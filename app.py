1534c713ef23df08d757ba7cefa2fc317aaa2588
Ali Fattah <alifattah.dev@gmail.com>
2025-05-16 19:17:20 +0330

    my first python code!

---
+++ app.py
@@ -0,0 +1,10 @@
+# print("Hello World")
+# student_count = 1000
+# # print(student_count)
+# course = "Python Course"
+# print(len(course))
+# print(course[0:3])
+
+age = int(input("Enter UR age: "))
+message = "U can enter!" if age >= 18 else "NO enter!!"
+print(message)
