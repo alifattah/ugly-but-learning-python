a11ef8b7275be7417d5abbd2f7e21257a861a6b7
DigitalGuy <alifattah.dev@gmail.com>
2025-05-17 11:19:22 +0330

    Iterables Objects

--- app.py
+++ app.py
@@ -5,17 +5,23 @@
 # print(len(course))
 # print(course[0:3])
 
-age = int(input("Enter UR age: "))
-message = "U can enter!" if age >= 18 else "NO enter!!"
-print(message)
+#age = int(input("Enter UR age: "))
+#message = "U can enter!" if age >= 18 else "NO enter!!"
+#print(message)
 
-f_name = input("Enter UR name: ")
-l_name = input("Enter UR last name: ")
-print(f"UR name is {f_name} & UR last name is {l_name} \nU R {age} years old")
+#f_name = input("Enter UR name: ")
+#l_name = input("Enter UR last name: ")
+#print(f"UR name is {f_name} & UR last name is {l_name} \nU R {age} years old")
 
-if len(f_name) >= 8:
-  print("Wowww! Such long name!")
-elif 5 < len(f_name) < 8:
-  print("Nice name")
-else:
-  print("OMG!!")
+#if len(f_name) >= 8:
+#    print("Wowww! Such long name!")
+#elif 5 < len(f_name) < 8:
+#    print("Nice name")
+#else:
+#    print("OMG!!")
+
+# Nested Loops
+
+for x in range(5):
+  for y in range(3):
+    print(f"({x}, {y})")
