a13f83aa3db764daa80b337887cd97dea6048198
DigitalGuy <alifattah.dev@gmail.com>
2025-05-29 15:03:33 +0330

    Nothing!

--- app.py
+++ app.py
@@ -1,94 +1,108 @@
+odd_even_number = int(input("enter a number: "))
 
-# *** My First Python Code With Mosh *** #
-# print("Hello World")
-# student_count = 1000
-# print(student_count)
-# course = "Python Course"
-# print(len(course))
-# print(course[0:3])
-
-#age = int(input("Enter UR age: "))
-#message = "U can enter!" if age >= 18 else "NO enter!!"
-#print(message)
-
-#f_name = input("Enter UR name: ")
-#l_name = input("Enter UR last name: ")
-#print(f"UR name is {f_name} & UR last name is {l_name} \nU R {age} years old")
+if odd_even_number % 2 == 0:
+    print(f"{odd_even_number} is ODD!")
+else:
+    print(f"{odd_even_number} is EVEN!!")
 
 
-# *** if Statement 
-#if len(f_name) >= 8:
-#    print("Wow! Such long name!")
-#elif 5 < len(f_name) < 8:
-#    print("Nice name")
-#else:
-#    print("OMG!!")
+for num in range(1, 11):
+    print(num)
 
-# Nested Loops
-
-# for x in range(5):
-#   for y in range(3):
-#     print(f"({x}, {y})")
-
-# passcode = ""
-
-# while passcode != "1234":
-#   passcode = input("Enter UR pass: ")
-# print(f"UR passcode is {passcode}!")
+while name != "exit":
+    input("ur name?")
 
 
+# # *** My First Python Code With Mosh *** #
+# # print("Hello World")
+# # student_count = 1000
+# # print(student_count)
+# # course = "Python Course"
+# # print(len(course))
+# # print(course[0:3])
 
-# i = 1
-# while i <= 5:
-#     print(i)
-#     i += 1
-# count = 0
-# for i in range(1, 10):
-#   if i % 2 == 0:
-#     count += 1
-#     print(i)
-# print(f"we have {count} even nambers")
+# #age = int(input("Enter UR age: "))
+# #message = "U can enter!" if age >= 18 else "NO enter!!"
+# #print(message)
 
-# age = int(input("Enter UR age: "))
-# if age > 18:
-#  print(f"Wow!")
-# elif 15 <=  age <= 18:
-#   print (f"Hmm!")
-# else:
-#  print(f"Hahaha")
-
-def get_greeting(name = "guest"):
-  return f"Hi {name}"
-
-message = get_greeting("Ali")
-
-print(message)
+# #f_name = input("Enter UR name: ")
+# #l_name = input("Enter UR last name: ")
+# #print(f"UR name is {f_name} & UR last name is {l_name} \nU R {age} years old")
 
 
+# # *** if Statement
+# #if len(f_name) >= 8:
+# #    print("Wow! Such long name!")
+# #elif 5 < len(f_name) < 8:
+# #    print("Nice name")
+# #else:
+# #    print("OMG!!")
 
-#message = get_greeting("Ali")
-#print(message)
+# # Nested Loops
 
-#file = open("content.txt", "w")
-#file.write(message)
+# # for x in range(5):
+# #   for y in range(3):
+# #     print(f"({x}, {y})")
 
-#f_name = input("nter your name")
-#l_name = input("enter tour last name")
+# # passcode = ""
 
-#def con_to_fullname(f_name, l_name):
- # return f"your full name is {f_name} {l_name}"
-
-#fullname = con_to_fullname(f_name , l_name)
-
-#print(fullname)
+# # while passcode != "1234":
+# #   passcode = input("Enter UR pass: ")
+# # print(f"UR passcode is {passcode}!")
 
 
-def multiply(*numbers):
+# # i = 1
+# # while i <= 5:
+# #     print(i)
+# #     i += 1
+# # count = 0
+# # for i in range(1, 10):
+# #   if i % 2 == 0:
+# #     count += 1
+# #     print(i)
+# # print(f"we have {count} even nambers")
 
-  total = 1
+# # age = int(input("Enter UR age: "))
+# # if age > 18:
+# #  print(f"Wow!")
+# # elif 15 <=  age <= 18:
+# #   print (f"Hmm!")
+# # else:
+# #  print(f"Hahaha")
 
-  for number in numbers:
-    total *= number
-    print(total)
+# def get_greeting(name = "guest"):
+#   return f"Hi {name}"
 
-(multiply(2, 3, 4, 5))
+# message = get_greeting("Ali")
+
+# print(message)
+
+
+# #message = get_greeting("Ali")
+# #print(message)
+
+# #file = open("content.txt", "w")
+# #file.write(message)
+
+# #f_name = input("enter your name")
+# #l_name = input("enter your last name")
+
+# #def con_to_fullname(f_name, l_name):
+#  # return f"your full name is {f_name} {l_name}"
+
+# #fullname = con_to_fullname(f_name , l_name)
+
+# #print(fullname)
+
+
+# def multiply(*numbers):
+
+#   total = 1
+
+#   for number in numbers:
+#     total *= number
+#     print(total)
+
+# (multiply(2, 3, 4, 5))
+
+# Keep asking the user for their name until they type "exit".
